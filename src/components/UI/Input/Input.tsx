6d23e1cae4a021f3e8488c5263078bce161c4e93
Mert Koseoglu <info@mkoseoglu.com>
2018-10-09 23:57:05 +0300

    Input UI component

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -1,31 +1,33 @@
-import classNames from "classnames";
-import * as React from "react";
-
-import "./input.scss";
+import classNames from 'classnames'
+import * as React from 'react'
+import './input.css'
 
 interface IProps {
-	className?: string;
-	color?: string;
-	type?: string;
-	placeholder?: string;
-	onChange?: (e: any) => void;
-	value?: string;
-	id?: string;
+	className?: string
+	color?: string
+	type?: string
+	placeholder?: string
+	onChange?: (e: any) => void
+	value?: string | number
+	id?: string
 }
 
 const Input: React.SFC<IProps> = (props: IProps) => {
-	const inputClasses = classNames({
-		"tm-input": true,
-		[`tm-input-${props.color}`]: true,
-		...(props.className ? props.className.split(" ") : "")
-	});
+	const inputClasses = classNames(
+		{
+			'tm-input': true,
+			[`tm-input-${props.color}`]: true,
+			...(props.className ? props.className.split(' ') : ''),
+		},
+		props.className
+	)
 
-	return <input {...props} className={inputClasses} />;
-};
+	return <input {...props} className={inputClasses} />
+}
 
 Input.defaultProps = {
-	color: "default",
-	type: "text"
-};
+	color: 'default',
+	type: 'text',
+}
 
-export default Input;
+export default Input
