3498fdddc1878b4e2db0afd5bba16c9c06097b9f
Mert Koseoglu <info@mkoseoglu.com>
2018-10-09 23:58:05 +0300

    Notify UI component

--- src/components/UI/Notify/Notify.tsx
+++ src/components/UI/Notify/Notify.tsx
@@ -1,31 +1,31 @@
-import * as React from "react";
-import classnames from "classnames";
-import INotify from "./INotify";
+import * as React from 'react'
+import classnames from 'classnames'
+import INotify from './INotify'
 
-import "./notify.scss";
+import './notify.scss'
 
 export interface IProps {
-	notifications?: INotify[];
+	notifications?: INotify[]
 }
 
 export default class Notify extends React.PureComponent<IProps> {
 	public render(): JSX.Element | null {
-		const { notifications = [] } = this.props;
+		const { notifications = [] } = this.props
 		return (
 			<div className="tm-notify-container">
 				{notifications.reverse().map((notification: INotify) => {
 					const className = classnames({
-						"tm-notify": true,
-						error: notification.color === "error" || notification.color === "red"
-					});
+						'tm-notify': true,
+						error: notification.color === 'error' || notification.color === 'red',
+					})
 					return (
 						<div key={notification.id} className={className}>
 							<h3>{notification.title}</h3>
 							<p>{notification.message}</p>
 						</div>
-					);
+					)
 				})}
 			</div>
-		);
+		)
 	}
 }
