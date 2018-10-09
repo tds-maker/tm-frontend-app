33361cd85d2a65f2d7c72469837f6a24e395cca8
Mert Koseoglu <info@mkoseoglu.com>
2018-10-09 23:47:12 +0300

    CheckBox UI component

--- src/components/UI/CheckBox/CheckBox.tsx
+++ src/components/UI/CheckBox/CheckBox.tsx
@@ -1,30 +1,41 @@
-import classNames from "classnames";
-import * as React from "react";
-
-import "./checkbox.scss";
+import classNames from 'classnames'
+import * as React from 'react'
+import './checkbox.css'
 
 interface IProps {
-	label?: string;
-	checked: boolean;
-	onChange?: ((e: any) => void);
-	id?: string;
-	color?: string;
-	rounded?: boolean;
+	label?: string
+	checked: boolean
+	onChange?: ((e: any) => void)
+	id?: string
+	color?: string
+	rounded?: boolean
+	style?: string
 }
 
-const CheckBox: React.SFC<IProps> = ({ label, checked, onChange, id, color = "primary", rounded = false }: IProps) => {
-	const className = classNames({
-		"tm-checkbox": true,
-		[color]: true,
-		rounded
-	});
+const CheckBox: React.SFC<IProps> = ({
+	label,
+	checked,
+	onChange,
+	id,
+	style,
+	color = 'primary',
+	rounded = false,
+}: IProps) => {
+	const className = classNames(
+		{
+			'tm-checkbox': true,
+			[color]: true,
+			rounded,
+		},
+		style
+	)
 
 	return (
 		<label className={className}>
 			<input id={id} type="checkbox" checked={checked} onChange={onChange} />
 			<span>{label}</span>
 		</label>
-	);
-};
+	)
+}
 
-export default CheckBox;
+export default CheckBox
