832043387789ac3cb4a2e488c977b63f0fd7459e
Mert Koseoglu <info@mkoseoglu.com>
2018-10-09 23:50:36 +0300

    FontFamily UI component

---
+++ src/components/UI/FontFamily/mockFonts.ts
@@ -0,0 +1,6 @@
+export default [
+	{ value: 'arial', name: 'Arial' },
+	{ value: 'arial_black', name: 'Arial Black' },
+	{ value: 'avenir_roman', name: 'Avenir Roman' },
+	{ value: 'futura_bold', name: 'Futura Bold' },
+]
