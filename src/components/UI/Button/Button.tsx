ea9378eabd04c0738c32241b0cf8192ba4b1402a
Mert Koseoglu <info@mkoseoglu.com>
2018-10-09 23:46:50 +0300

    Button UI component

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,44 +1,31 @@
-import "./button.scss";
-
-import classNames from "classnames";
-import * as React from "react";
+import classNames from 'classnames'
+import * as React from 'react'
+import './button.css'
 
 interface IProps {
-	color?: string;
-	className?: string;
-	children?: any;
-	id?: string;
-	size?: string;
-	onClick?: ((e: any) => void);
-	disabled?: boolean;
-	type?: string;
-	style?: object;
-	renderAs?: string;
+	color?: string
+	className?: string
+	children?: any
+	id?: string
+	size?: string
+	onClick?: ((e: any) => void)
+	disabled?: boolean
+	type?: string
 }
 
-const getClasses = (props: IProps) =>
-	classNames({
-		"tm-btn": true,
-		"tm-btn-small": props.size && props.size === "small" ? true : false,
-		[`tm-btn-${props.color}`]: props.color ? true : false,
-		[props.className || ""]: true
-	});
-
-const renderAsAnchor = (props: IProps) => (
-	<a id={props.id} onClick={props.onClick} style={props.style} className={getClasses(props)}>
-		{props.children}
-	</a>
-);
-
-const renderAsButton = (props: IProps) => (
-	<button id={props.id} onClick={props.onClick} style={props.style} className={getClasses(props)} type={props.type || "button"}>
-		{props.children}
-	</button>
-);
-
 const Button: React.SFC<IProps> = (props: IProps) => {
-	const renderAs = props.renderAs || "button";
-	return renderAs === "button" ? renderAsButton(props) : renderAsAnchor(props);
-};
+	const classes = classNames({
+		'tm-btn': true,
+		'tm-btn-small': props.size && props.size === 'small' ? true : false,
+		[`tm-btn-${props.color}`]: props.color ? true : false,
+		[props.className || '']: true,
+	})
 
-export default Button;
+	return (
+		<button {...props} className={classes}>
+			{props.children}
+		</button>
+	)
+}
+
+export default Button
