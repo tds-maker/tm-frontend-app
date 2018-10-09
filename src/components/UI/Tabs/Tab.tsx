adc588d80bf67521c128bcc200722ae9340ba820
Mert Koseoglu <info@mkoseoglu.com>
2018-10-10 00:01:51 +0300

    Tabs UI component

--- src/components/UI/Tabs/Tab.tsx
+++ src/components/UI/Tabs/Tab.tsx
@@ -1,18 +1,14 @@
-import * as React from 'react';
+import * as React from 'react'
 
 interface IProps {
-  header?: string;
-  className?: string;
+	header?: string
+	className?: string
 }
 
 class Tab extends React.PureComponent<IProps> {
-  public render(): JSX.Element {
-    return (
-      <div className={`tabs_content ${this.props.className}`}>
-        {this.props.children}
-      </div>
-    );
-  }
+	public render(): JSX.Element {
+		return <div className={`tabs_content ${this.props.className}`}>{this.props.children}</div>
+	}
 }
 
-export default Tab;
+export default Tab
