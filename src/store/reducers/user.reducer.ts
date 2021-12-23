d97305f24cb3294e6567bfd1a55c2fbf14e0f9cb
Melih Korkmaz <melihkorkmaz84@gmail.com>
2021-12-23 09:34:02 +0100

    remove token

--- src/store/reducers/user.reducer.ts
+++ src/store/reducers/user.reducer.ts
@@ -10,7 +10,7 @@ const defaultState: IUserStore = {
 	firstName: 'Melih',
 	lastName: 'Korkmaz',
 	token:
-		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImZha2V1c2VyaWQiLCJhY2NvdW50SWQiOiJBMTIzIiwiaWF0IjoxNTMwMTc3NDgyfQ.N73feinTiO8tuBvpLL5rMbLxWj7Odsp-DEJEQqzsn5w',
+		'',
 }
 
 // Change this
